61775eeb2873498f6d8087c4dfba21b850a00126
P <tatakornaow@gmail.com>
2023-01-13 16:00:52 +0700

    done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,43 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[])
+{ 
+    for(int i=0;i<n;i++)
+    {
+        b[0]+=a[i];
+    }
+    b[0]=b[0]/n;
+    
+    for(int i=0;i<n;i++)
+    {
+        b[1] += pow((a[i]-b[0]),2);
+    }
+    b[1] = sqrt(b[1]/n);
+
+    b[2] = 1;
+    for(int i=0;i<n;i++)
+    {
+        
+        b[2] *= a[i];
+    }
+    b[2] = pow(b[2],1.0/n);
+
+    for(int i=0;i<n;i++)
+    {
+        b[3] += 1/a[i];
+    }
+    b[3] = n/b[3];
+
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>b[4]) b[4]=a[i];
+    }
+    
+    b[5]=a[1];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]<b[5]) b[5]=a[i];
+    }
+}
